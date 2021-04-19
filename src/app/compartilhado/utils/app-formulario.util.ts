6883f3da485d92f22c71c35cfe24e762fce71b6c
Jonathan Moraes <jonathan.ssm@hotmail.com>
2021-04-19 17:20:59 -0300

    v1.0.2
    ajustando componente input

--- src/app/compartilhado/utils/app-formulario.util.ts
+++ src/app/compartilhado/utils/app-formulario.util.ts
@@ -4,11 +4,12 @@ export class AppFormularioUtil {
 
     /*
     * A partir do FormControl passado por parametro e recuperado o nome do mesmo
+    *
     * @param control
     */
     public static getNomeFormControl(control: FormControl): string {
         let nomeFormControl = '';
-        const parent = control.get('_parent');
+        const parent = control.parent;
 
         // somente o 'parent' do tipo FormGroup e q possui as informacoes sobre o nome do FormControl
         if (parent instanceof FormGroup) {
