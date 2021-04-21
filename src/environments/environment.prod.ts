93fc06adb335b676eb6a6642b6608e23499100ef
Jonathan Moraes <jonathan.ssm@hotmail.com>
2021-04-21 08:02:29 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAyFlTObXGVI_f9MIYjWUjfsgLFxB4BkNk',
+    authDomain: 'test-wktec.firebaseapp.com',
+    projectId: 'test-wktec',
+    storageBucket: 'test-wktec.appspot.com',
+    messagingSenderId: '811919747776',
+    appId: '1:811919747776:web:f613c2dd1a19d4b09b1b25',
+    measurementId: 'G-6X0SK66BKK'
+  }
 };
